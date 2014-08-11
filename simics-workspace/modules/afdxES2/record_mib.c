c805b1783d2ffb78a453a2a498974b1ec672976f
Tyrion Lannister <Tyrion.Lannister.274AL@gmail.com>
2014-08-11 20:57:37 +0800

    diff --git a/simics-workspace/modules/afdxES2/record_mib.c b/simics-workspace/modules/afdxES2/record_mib.c index e95522c..ae81cf7 100644 --- a/simics-workspace/modules/afdxES2/record_mib.c +++ b/simics-workspace/modules/afdxES2/record_mib.c @@ -1,8 +1,8 @@  #include "record_mib.h"
     void write_mid(MAC_MIB *maca_mib, MAC_MIB *macb_mib, RX_MIB *rx_mib, TX_MIB *tx_mib) {
    -    FILE *mib_file = fopen("./mib_file.conf", "w");    // look at here!
    -    fprintf(mib_file, "========================================= Mac A mib begin ===================================================");
    +    FILE *mib_file = fopen("./mib_file.conf", "w");
    +    fprintf(mib_file, "========================================= Mac A mib begin ===================================================\n");
         fprintf(mib_file, "crc_error: %d\n", maca_mib->crc_error);
         fprintf(mib_file, "delete_little_64: %d\n", maca_mib->delete_little_64);
         fprintf(mib_file, "delete_big_1518: %d\n", maca_mib->delete_big_1518);
    @@ -24,7 +24,7 @@ void write_mid(MAC_MIB *maca_mib, MAC_MIB *macb_mib, RX_MIB *rx_mib, TX_MIB *tx_
         fprintf(mib_file, "ipchecksum_error_count: %d\n", maca_mib->ipchecksum_error_count);
         fprintf(mib_file, "delete_bit_2048_count: %d\n", maca_mib->delete_bit_2048_count);
    
    -    fprintf(mib_file, "========================================= Mac B mib begin ===================================================");
    +    fprintf(mib_file, "========================================= Mac B mib begin ===================================================\n");
         fprintf(mib_file, "crc_error: %d\n", macb_mib->crc_error);
         fprintf(mib_file, "delete_little_64: %d\n", macb_mib->delete_little_64);
         fprintf(mib_file, "delete_big_1518: %d\n", macb_mib->delete_big_1518);
    @@ -47,10 +47,10 @@ void write_mid(MAC_MIB *maca_mib, MAC_MIB *macb_mib, RX_MIB *rx_mib, TX_MIB *tx_
         fprintf(mib_file, "delete_bit_2048_count: %d\n", macb_mib->delete_bit_2048_count);
    
    -    fprintf(mib_file, "========================================= rx mib begin =====================================================");
    +    fprintf(mib_file, "========================================= rx mib begin =====================================================\n");
         fprintf(mib_file, "disable_A_filt_count: %d\n", rx_mib->disable_A_filt_count);
         fprintf(mib_file, "disable_B_filt_count: %d\n", rx_mib->disable_B_filt_count);
    -    fprintf(mib_file, "A_integrated_filt_count: d\n", rx_mib->A_integrated_filt_count);
    +    fprintf(mib_file, "A_integrated_filt_count: %d\n", rx_mib->A_integrated_filt_count);
         fprintf(mib_file, "B_integrated_filt_count: %d\n", rx_mib->B_integrated_filt_count);
         fprintf(mib_file, "vl_id_filt_count: %d\n", rx_mib->vl_id_filt_count);
         fprintf(mib_file, "redunt_filt_count: %d\n", rx_mib->redunt_filt_count);
    @@ -68,7 +68,7 @@ void write_mid(MAC_MIB *maca_mib, MAC_MIB *macb_mib, RX_MIB *rx_mib, TX_MIB *tx_
         fprintf(mib_file, "rx_icmp_big_max_count: %d\n", rx_mib->rx_icmp_big_max_count);
         fprintf(mib_file, "rx_buffer_clear_delete_count: %d\n", rx_mib->rx_buffer_clear_delete_count);
    
    -    fprintf(mib_file, "========================================= tx mib begin =====================================================");
    +    fprintf(mib_file, "========================================= tx mib begin =====================================================\n");
         fprintf(mib_file, "tx_ip_count: %d\n", tx_mib->tx_ip_count);
         fprintf(mib_file, "tx_udp_count: %d\n", tx_mib->tx_udp_count);
         fprintf(mib_file, "tx_icmp_count: %d\n", tx_mib->tx_icmp_count);

--- simics-workspace/modules/afdxES2/record_mib.c
+++ simics-workspace/modules/afdxES2/record_mib.c
@@ -1,8 +1,8 @@
 #include "record_mib.h"
 
 void write_mid(MAC_MIB *maca_mib, MAC_MIB *macb_mib, RX_MIB *rx_mib, TX_MIB *tx_mib) {
-    FILE *mib_file = fopen("./mib_file.conf", "w");    // look at here!
-    fprintf(mib_file, "========================================= Mac A mib begin ===================================================");
+    FILE *mib_file = fopen("./mib_file.conf", "w");    
+    fprintf(mib_file, "========================================= Mac A mib begin ===================================================\n");
     fprintf(mib_file, "crc_error: %d\n", maca_mib->crc_error);
     fprintf(mib_file, "delete_little_64: %d\n", maca_mib->delete_little_64);
     fprintf(mib_file, "delete_big_1518: %d\n", maca_mib->delete_big_1518);
@@ -24,7 +24,7 @@ void write_mid(MAC_MIB *maca_mib, MAC_MIB *macb_mib, RX_MIB *rx_mib, TX_MIB *tx_
     fprintf(mib_file, "ipchecksum_error_count: %d\n", maca_mib->ipchecksum_error_count);
     fprintf(mib_file, "delete_bit_2048_count: %d\n", maca_mib->delete_bit_2048_count);
 	
-    fprintf(mib_file, "========================================= Mac B mib begin ===================================================");
+    fprintf(mib_file, "========================================= Mac B mib begin ===================================================\n");
     fprintf(mib_file, "crc_error: %d\n", macb_mib->crc_error);
     fprintf(mib_file, "delete_little_64: %d\n", macb_mib->delete_little_64);
     fprintf(mib_file, "delete_big_1518: %d\n", macb_mib->delete_big_1518);
@@ -47,10 +47,10 @@ void write_mid(MAC_MIB *maca_mib, MAC_MIB *macb_mib, RX_MIB *rx_mib, TX_MIB *tx_
     fprintf(mib_file, "delete_bit_2048_count: %d\n", macb_mib->delete_bit_2048_count);
 	
 	
-    fprintf(mib_file, "========================================= rx mib begin =====================================================");
+    fprintf(mib_file, "========================================= rx mib begin =====================================================\n");
     fprintf(mib_file, "disable_A_filt_count: %d\n", rx_mib->disable_A_filt_count);
     fprintf(mib_file, "disable_B_filt_count: %d\n", rx_mib->disable_B_filt_count);
-    fprintf(mib_file, "A_integrated_filt_count: d\n", rx_mib->A_integrated_filt_count);
+    fprintf(mib_file, "A_integrated_filt_count: %d\n", rx_mib->A_integrated_filt_count);
     fprintf(mib_file, "B_integrated_filt_count: %d\n", rx_mib->B_integrated_filt_count);
     fprintf(mib_file, "vl_id_filt_count: %d\n", rx_mib->vl_id_filt_count);
     fprintf(mib_file, "redunt_filt_count: %d\n", rx_mib->redunt_filt_count);
@@ -68,7 +68,7 @@ void write_mid(MAC_MIB *maca_mib, MAC_MIB *macb_mib, RX_MIB *rx_mib, TX_MIB *tx_
     fprintf(mib_file, "rx_icmp_big_max_count: %d\n", rx_mib->rx_icmp_big_max_count);
     fprintf(mib_file, "rx_buffer_clear_delete_count: %d\n", rx_mib->rx_buffer_clear_delete_count);
 	
-    fprintf(mib_file, "========================================= tx mib begin =====================================================");
+    fprintf(mib_file, "========================================= tx mib begin =====================================================\n");
     fprintf(mib_file, "tx_ip_count: %d\n", tx_mib->tx_ip_count);
     fprintf(mib_file, "tx_udp_count: %d\n", tx_mib->tx_udp_count);
     fprintf(mib_file, "tx_icmp_count: %d\n", tx_mib->tx_icmp_count);
