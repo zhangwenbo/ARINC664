2f326f3f30eb0038920e6be0949592ee3a42d6ab
Tyrion Lannister <Tyrion.Lannister.274AL@gmail.com>
2014-09-02 14:13:32 +0800

    del rtc_module

--- simics-workspace/modules/rtc_module/test/s-rtc_module.py
+++
@@ -1,9 +0,0 @@
-import dev_util
-import conf
-import stest
-import rtc_module_common
-
-# Create an instance of the device to test
-dev = rtc_module_common.create_rtc_module()
-
-# Write your tests here
